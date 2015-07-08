78881a7c7f738c40379c538e88ac47b0ede87a1a
Giovanni Garufi <giovanni.garufi@fwr.it>
2015-07-08 16:23:01 +0200

    ammend this message

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-var lastPlayingTab;
+var lastActiveTab;
 var queryInfo = {
   url: '*://soundcloud.com/*'
 };
@@ -12,13 +12,13 @@ function prepareScript (scriptFile) {
 }
 
 function isTabPlaying (tab, callback) {
-  getTabInfo(tab, function(tabInfo) {
+  getTabInfo(tab.id, function(tabInfo) {
     return callback(tabInfo.playing);
   });
 }
 
-function getTabInfo (tab, callback) {
-  chrome.tabs.executeScript(tab.id, prepareScript('getTabInfo'), function(results) {
+function getTabInfo (tabId, callback) {
+  chrome.tabs.executeScript(tabId, prepareScript('getTabInfo'), function(results) {
     return callback(results[0]);
   });
 }
@@ -27,19 +27,19 @@ function findSuitableTab (tabs, callback) {
   return async.detect(tabs, isTabPlaying, function(playingTab) {
      if (playingTab) {
        //update playing tab
-       lastPlayingTab = playingTab;
+       lastActiveTab = playingTab;
 
        return callback(playingTab);
      } else {
-       // if no tab is playing return lastPlayingTab
-       if (lastPlayingTab) {
-         return callback(lastPlayingTab);
+       // if no tab is playing return lastActiveTab
+       if (lastActiveTab) {
+         return callback(lastActiveTab);
        } else {
          //if all else fails just return first tab found
          var pausedTab = tabs[0];
 
          // save as lastplaying tab
-         lastPlayingTab = pausedTab;
+         lastActiveTab = pausedTab;
          return callback(pausedTab);
        }
      }
@@ -48,15 +48,19 @@ function findSuitableTab (tabs, callback) {
 
 function findSoundCloundTabId (callback) {
   chrome.tabs.query(queryInfo, function(tabs) {
-    var tab;
 
     if (tabs.length === 0) return callback();
 
-    if (tabs.length === 1) return callback(tabs[0].id);
+    if (tabs.length === 1) {
+      var tab = tabs[0];
+
+      lastActiveTab = tab;
+      return callback(tab.id);
+    }
 
     else {
-      findSuitableTab(tabs, function(playingTab) {
-        return callback(playingTab.id);
+      findSuitableTab(tabs, function(tab) {
+        return callback(tab.id);
       });
     }
 
@@ -75,16 +79,29 @@ function executeCommand(command) {
 
   findSoundCloundTabId(function(playingTabId) {
     if (playingTabId) {
-      chrome.tabs.executeScript(playingTabId, prepareScript(parsedCommand));
-      emitPageStatus();
+      chrome.tabs.executeScript(playingTabId, prepareScript(parsedCommand), function() {
+        emitPageStatus();
+      });
+    } else if (lastActiveTab) {
+
+      var createProperties = {
+        url: lastActiveTab.url,
+        active: false
+      };
+      chrome.tabs.create(createProperties, function(newTab) {
+
+      });
+    }
+    else {
+      console.log('Not tab or lastActiveTab found :(');
     }
   });
 }
 
 function emitPageStatus () {
-  chrome.tabs.query(queryInfo, function(tabs) {
-    findSuitableTab(tabs, function(tab) {
-      getTabInfo(tab, function(tabInfo) {
+  findSoundCloundTabId(function(tabId) {
+    if (tabId) {
+      getTabInfo(tabId, function(tabInfo) {
         soundCloudStatus.playing = tabInfo.playing;
         soundCloudStatus.repeating = tabInfo.repeating;
         soundCloudStatus.muted = tabInfo.muted;
@@ -92,7 +109,7 @@ function emitPageStatus () {
 
         chrome.runtime.sendMessage(soundCloudStatus);
       });
-    });
+    }
   });
 }
 
